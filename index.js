5f17e271619e7e3032000bda029b9c9ab548a96c
Andrew Simmons <neon.duncan@gmail.com>
2020-12-03 23:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return parseInt(num1 / num2)
 }
+
+function increment(n) {
+  return n++
+}
+
+function decrement(n) {
+  return n--
+}
